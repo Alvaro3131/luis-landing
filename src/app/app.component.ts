4529612e5f0d51367bb583e2fa3112f6731191bf
Alvaro3131 <73666415+Alvaro3131@users.noreply.github.com>
2024-01-10 20:41:02 -0500

    Story 182: Fix benchmarks for speed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { animate, query, stagger, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: [
+    trigger('slideInFromTop', [
+      state('void', style({ transform: 'translateY(-100%)', opacity: 0 })),
+      transition('void => *', [
+        animate('500ms ease-in-out', style({ transform: 'translateY(0)', opacity: 1 }))
+      ]),
+    ]),
+  ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+  }
   title = 'landing-page';
 }
